fcd7bd101b1536bed5f29bbe5bd80f7e75595b88
karakune <hugopelletier783@gmail.com>
2025-02-10 15:14:59 -0500

    First android build

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,6 @@ import Minion from "./Components/Minion.tsx";
 function App() {
     return (
         <main className="container">
-            <div className="menu-zone">
-                <button>a
-                    {/*<img src={burgerStack} alt="burger stack"/>*/}
-                </button>
-            </div>
             <div className="minion-zone">
                 <Minion name="Steel Defender" mainColor="coral" subColor="lightcoral"/>
                 <Minion name="Homunculus Servant" mainColor="royalblue" subColor="cornflowerblue"/>
